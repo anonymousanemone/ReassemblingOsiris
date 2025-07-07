c52d6e6fc87f6b6a93be05e7c363230fcce097cb
Sophia <sosophia0702@gmail.com>
2025-07-07 12:35:11 -0400

    saving progress

---
+++ DCproject.materials/Release-stable/hoverfilllink.js
@@ -0,0 +1,39 @@
+
+function HoverFillLink(event) {
+  const outputArea = document.getElementById("vorple");
+
+  if (!outputArea) {
+    console.error("Required element not found.");
+    return;
+  }
+
+  outputArea.addEventListener("mouseover", function (event) {
+    const link = event.target.closest("a.vorple-commandlink");
+    if (link && outputArea.contains(link)) {
+      const command = link.getAttribute("data-command");
+      console.log("Hovered command:", command);
+      if (command) {
+        vorple.prompt.setValue(command);
+      }
+      const input = document.getElementById("lineinput-field");
+      // console.log(input)
+      if (input) {
+        input.style.color = "var(--prefill-color)";
+      }
+    }
+  });
+
+  outputArea.addEventListener("mouseout", function (event) {
+    const link = event.target.closest("a.vorple-commandlink");
+    if (link && outputArea.contains(link)) {
+      const input = document.getElementById("lineinput-field");
+      if (input) {
+        vorple.prompt.setValue("");
+        input.style.color = input.dataset.originalColor || "";
+      }
+    }
+  });
+}
+
+vorple.addEventListener("init", HoverFillLink);
+
